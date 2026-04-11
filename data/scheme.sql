f3a4202a2fa476e28a62a64bf82d130a84722707
Gustav Sönnichsen <gustavsoennichsen@icloud.com>
2026-04-11 13:24:00 -0300

    feat: update database schema to include routes and stops tables with additional fields

--- data/scheme.sql
+++ data/scheme.sql
@@ -39,3 +39,22 @@ CREATE TABLE IF NOT EXISTS bus_progress (
     current_stop_index INTEGER,
     last_update TEXT
 );
+
+CREATE TABLE IF NOT EXISTS routes (
+    route_id INT PRIMARY KEY,
+    bus_id INT,           -- Foreign key referencing the bus
+    start_time TIME,      -- Optional: Start time for the route
+    end_time TIME,        -- Optional: End time for the route
+);
+
+CREATE TABLE IF NOT EXISTS stops (
+    stop_id INT PRIMARY KEY,
+    stop_location VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS route_stops (
+    route_id INT,               -- Foreign key to routes table
+    stop_id INT,                -- Foreign key to stops table
+    stop_order INT,             -- The order of the stop in the route
+    PRIMARY KEY (route_id, stop_id)
+);
